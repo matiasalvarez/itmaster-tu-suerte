e03c0113f7406aaaa5e0b604743b9ad4f1d2248c
Matias Alvarez <alvarezmatiasezequiel@yahoo.com.ar>
2016-05-15 17:39:50 -0300

    Botones compartir y cambio de predicciones implementado

--- app/src/main/java/com/pisys/tusuerte/PrediccionActivity.java
+++ app/src/main/java/com/pisys/tusuerte/PrediccionActivity.java
@@ -1,8 +1,12 @@
 package com.pisys.tusuerte;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -33,7 +37,7 @@ public class PrediccionActivity extends AppCompatActivity {
 
         tvPrediccion = (TextView) findViewById(R.id.tvPrediccion);
         tvPrediccion.setMovementMethod(ScrollingMovementMethod.getInstance());
-        seleccionarAmor();
+        seleccionarAmor(null);
 
         TextView tvNombre = (TextView) findViewById(R.id.tvNombreSigno);
         tvNombre.setText(titulo);
@@ -42,7 +46,50 @@ public class PrediccionActivity extends AppCompatActivity {
         ivImagen.setImageResource(foto);
     }
 
-    private void seleccionarAmor() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.prediccion_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String prediccion = tvPrediccion.getText().toString();
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.enviarMail:
+                intent.setAction(Intent.ACTION_SENDTO);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Mirá la predicción de Tu Suerte!");
+                intent.putExtra(Intent.EXTRA_TEXT, prediccion);
+                intent.setData(Uri.parse("mailto:"));
+                startActivity(Intent.createChooser(intent, "Enviar Predicción por Email"));
+                return true;
+            case R.id.enviarOtro:
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, prediccion);
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, "Enviar Predicción..."));
+                return true;
+            case R.id.enviarSms:
+                intent.setAction(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("smsto:"));
+                intent.putExtra("sms_body", prediccion);
+                startActivity(Intent.createChooser(intent, "Enviar Predicción por SMS"));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void seleccionarSalud(MenuItem item) {
+        tvPrediccion.setText(prediccionSalud);
+    }
+
+    public void seleccionarDinero(MenuItem item) {
+        tvPrediccion.setText(prediccionDinero);
+    }
+
+    public void seleccionarAmor(MenuItem item) {
         tvPrediccion.setText(prediccionAmor);
     }
 }
